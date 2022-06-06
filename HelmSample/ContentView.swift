2c49fe28f68c1f3d779f00c7eaf796d0670794e1
Priyank Patel <priyank@rspndr.io>
2022-06-06 16:36:07 -0400

    Initial Commit

---
+++ HelmSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelmSample
+//
+//  Created by Priyank Patel on 2022-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelmSample/HelmSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelmSampleApp.swift
+//  HelmSample
+//
+//  Created by Priyank Patel on 2022-06-06.
+//
+
+import SwiftUI
+
+@main
+struct HelmSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
